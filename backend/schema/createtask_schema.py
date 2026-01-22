d8be48e74855b4a4781f8349412b36ae460d6e8c
abhishek-dev134 <abhishek.kumar2@anaptyss.com>
2026-01-22 19:58:48 +0530

    model / services / schema

--- backend/schema/createtask_schema.py
+++ backend/schema/createtask_schema.py
@@ -7,22 +7,17 @@ from typing_extensions import Literal
 
 class CategoryEnum(str, Enum):
     document_requirement = "Document Requirement"
-    # Add other categories here if needed
+ 
 
 
 class TaskCreate(BaseModel):
-    task_id: int = Field(..., alias="Task ID", example=675793)
-    date: date = Field(..., example="2025-07-11")
-    name: str = Field(..., example="Naresh Kumar")
+    id: int = Field(..., alias="Task ID")
+    created_at: date= Field()
+    created_by: str = Field()
     category: CategoryEnum = Field(..., example="Document Requirement")
     attach_file_url: Optional[str] = Field(None, description="URL or path to attached PDF file")
     description: Optional[str] = Field(
-        None,
-        example=(
-            "Please share the SMART Integrated Portal document, as it is a mandatory "
-            "requirement for verification and processing. Please ensure that the document "
-            "is complete and valid before submission to avoid any delays in the evaluation process."
-        )
+        None
     )
 
 
@@ -30,4 +25,4 @@ class TaskOut(TaskCreate):
     id: str
     created_at: date
     updated_at: date
-    source: Literal["manual", "system", "api"] = "manual"
+    source: Literal[None] = "manual"
